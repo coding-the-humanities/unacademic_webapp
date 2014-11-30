91c34ca5dbfaf634b28e9599a7168944ba1dee39
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2014-11-30 01:37:36 +0100

    fix nav-bar

--- src/app/app.js
+++ src/app/app.js
@@ -32,39 +32,75 @@ app.controller('Main', function(Path) {
 
   /* - CardCtrl- */
 
-  app.delete = function(path){
-    path.$delete().then(function(){
-      _.remove(app.paths, path);
-    });
-  }
+  app.remove= remove;
+  app.save = save;
 
-  app.save = function(path){
+  /* - CardsCtrl - */
+
+  app.viewActions = pathActions();
+  app.organizeActions = pathActions(true);
+
+  function save(path){
     path.$save().then(function(){
       path.creating = false;
     });
   };
 
-  /* - CardsCtrl - */
-
-  app.add = function(){
-    var path = Path.new({name: "new path", curator: "yeehaa", version: "0.0.0"});
-    path.creating = true;
-    app.paths.push(path);
-  }
-
-  app.organize = function(){
-    app.paths.map(function(path){
-      path.organizing = true;
-      return path;
+  function remove(path){
+    path.$delete().then(function(){
+      _.remove(app.paths, path);
     });
   }
 
-  app.clear = function(){
-    app.paths.forEach(function(path){
-      path.$delete();
-      app.paths = [];
-    });
-  }
+  function pathActions(organizing){
+
+    function add(){
+      var path = Path.new({name: "new path", curator: "yeehaa", version: "0.0.0"});
+      path.creating = true;
+      app.paths.push(path);
+    }
+
+    function organize(){
+      app.paths.map(function(path){
+        app.organizing = true;
+        path.organizing = true;
+        return path;
+      });
+    }
+
+    function done(){
+      app.paths.map(function(path){
+        app.organizing = false;
+        path.organizing = false;
+        return path;
+      });
+    }
+
+    function clear(){
+      app.paths.forEach(function(path){
+        path.$delete();
+        app.paths = [];
+      });
+    }
+
+    if(!organizing){
+      return {
+        add: add,
+        organize: organize,
+        clear: clear
+      }
+    } else {
+
+      return {
+        add: add,
+        done: done,
+        clear: clear
+      }
+    }
+
+  };
+
+
 });
 
 app.factory('Path', function(ActiveResource) {
@@ -72,19 +108,25 @@ app.factory('Path', function(ActiveResource) {
   function Path(data) {
     this.number('id');
     this.string('curator');
-    this.string('name');
+    this.string('title');
     this.string('description');
+    this.number('forked_from');
     this.string('version');
+    this.forks = data.forks;
+    this.places = data.places;
+    this.learners = data.learners;
+    this.isFork = !!data.forked_from;
+
 
     this.validates({
-      name: { presence: true },
+      title: { presence: true },
       curator: { presence: true },
       version: { presence: true },
     });
   };
 
   Path.inherits(ActiveResource.Base);
-  Path.api.set('http://cth.loc/api/0/paths');
+  Path.api.set('http://private-7c8dd-unacademic.apiary-mock.com');
 
   window.Path = Path;
 
